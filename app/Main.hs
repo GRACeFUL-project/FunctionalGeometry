25a18e38f04dc456a7d4885621b8bfe3300a8a40
Alex Gerdes <agerdes@me.com>
2018-03-09 17:18:59 +0100

    Adapted Main module to new interface and made filename and limit variable.

--- app/Main.hs
+++ app/Main.hs
@@ -1,18 +1,12 @@
 module Main where
 
-import Codec.Picture( PixelRGBA8( .. ), writePng )
-import Graphics.Rasterific
-import Graphics.Rasterific.Texture
-
-import Geometry as G
-import Control.Monad as M
+import Fish
+import Image
+import System.Environment
 
 main :: IO ()
 main = do
-    let white = PixelRGBA8 255 255 255 255
-        black = PixelRGBA8 0 0 0 255
-        img = renderDrawing 1000 1000 white $
-            withTexture (uniformTexture black) $ do
-                mconcat $ fmap (\b -> stroke 1 JoinRound (CapRound, CapRound) b) G.scaledImage
-    
-    writePng "test.png" img
+  args <- getArgs
+  case args of
+    [file, n] -> writeImage file $ squarelimit $ read n
+    _         -> writeImage "test.png" $ squarelimit 3
